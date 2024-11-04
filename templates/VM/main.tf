b89dc55e88006a0796483b809467bddeb16a39e7
dimitrisfinas <dimitris.finas@servicenow.com>
2024-11-04 17:16:41 +0100

    add name, region and zone as vars

--- templates/VM/main.tf
+++ templates/VM/main.tf
@@ -1,7 +1,8 @@
 resource "google_compute_instance" "vm-from-terraform" {
-  name         = "vm-from-terraform"
+  name         = var.name
   machine_type = "n2-standard-2"
-  zone         = "us-central1-a"
+  #zone         = "us-central1-a"
+  zone         = var.zone
 
   tags = ["foo", "bar"]
 
@@ -16,7 +17,6 @@ resource "google_compute_instance" "vm-from-terraform" {
 
   network_interface {
     network = "default"
-
   }
 
   metadata = {
@@ -24,5 +24,6 @@ resource "google_compute_instance" "vm-from-terraform" {
   }
 
   metadata_startup_script = "echo hi > /test.txt"
+  allow_stopping_for_update = true 
 
 }
--- templates/VM/variables.tf
+++ templates/VM/variables.tf
@@ -3,14 +3,25 @@ variable "credentials" {
     description = "Google Cloud credential"
 }
 
+variable "name" {
+    type = string
+    description = "Name of your Virtual Machine"
+}
+
 variable "project" {
     type = string
     description = "Google Cloud Project to deploy to"
-    default = "lightstep-cs"
+#    default = "lightstep-cs"
 }
 
 variable "region" {
     type = string
-    description = "Google Cloud region"
+    description = "Google Cloud Region"
     default = "us-central1"
+}
+
+variable "zone" {
+    type = string
+    description = "Google Cloud Zone"
+    default = "us-central1-a"
 }
